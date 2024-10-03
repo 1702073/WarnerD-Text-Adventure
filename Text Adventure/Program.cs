fd48553f5a156138e628a50b8cee81687c150641
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2024-10-03 13:32:02 -0700

    code stuff

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Text_Adventure
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Text_Adventure
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I will take over this imnifishiont world");
+            Console.WriteLine("I will take over this dying world");
+            Console.WriteLine("Will you struggle or die");
+            
+            string input = Console.ReadLine();
+            if (input == "Yes" || input == "Yes")
+            {
+                Console.WriteLine("");
+            }
+            else if(input.ToUpper() == "NO")
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+
+            }
         }
+
+        void Choice1()
+
     }
 }
