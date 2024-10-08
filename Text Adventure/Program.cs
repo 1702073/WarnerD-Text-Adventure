0c95a95e1f80b346f94687d737c0c3eb2c47581f
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2024-10-08 13:40:10 -0700

    stuff

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -4,6 +4,7 @@ namespace Text_Adventure
 {
     internal class Program
     {
+        public string playerName ;
         static void TypeLine(string line, int delay = 50)
         {
             for (int i = 0; i < line.Length; i++)
@@ -12,17 +13,44 @@ namespace Text_Adventure
                 Thread.Sleep(delay); // Sleep for 150 milliseconds
             }
         }
+
+        //static void TypeLine(string line, int delay = 50)
+        //{
+        //    for (int i = 0; i < line.Length; i++)
+        //    {
+        //        Console.Write(line[i]);
+        //        Thread.Sleep(delay); // Sleep for 150 milliseconds
+        //    }
+        //}
+
+
+
         static void Main(string[] args)
         {
-
             Random r = new Random();
-            int rInt = r.Next(9000, 10000);
+            int rInt = r.Next(60,110);
 
-            TypeLine("tamriel\r\n", rInt);
+
+            TypeLine("This is how i type is  it normal or to fast or too slow\r\n", rInt);
             Console.WriteLine(rInt);
-            TypeLine("tamriel\r\n", rInt);
-            rInt = r.Next(0, 10);
+
+            rInt = r.Next(60,110);
+            TypeLine("This is how i type is  it normal or to fast or too slow\r\n", rInt);
             Console.WriteLine(rInt);
+
+            rInt = r.Next(60,110);
+            TypeLine("This is how i type is  it normal or to fast or too slow\r\n", rInt);
+            Console.WriteLine(rInt);
+
+            rInt = r.Next(60,110);
+            TypeLine("This is how i type is  it normal or to fast or too slow\r\n", rInt);
+            Console.WriteLine(rInt);
+
+            rInt = r.Next(60,110);
+            TypeLine("This is how i type is  it normal or to fast or too slow\r\n", rInt);
+            Console.WriteLine(rInt);
+
+
 
             //Console.WriteLine("What is Your Name");
             //string playerName = Console.ReadLine();
@@ -32,19 +60,19 @@ namespace Text_Adventure
             //Console.WriteLine("Will you struggle or die");
 
 
-            //string input = Console.ReadLine();
-            //if (input == "Yes" || input == "Y" || input == "yes" || input == "YES" || input == "y")
-            //{
-            //    Console.WriteLine("Lame");
-            //}
-            //else if (input.ToUpper() == "NO" || input.ToUpper() == "N")
-            //{
-            //    Console.WriteLine("Dude y");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("tank");
-            //}
+            string input = Console.ReadLine();
+            if (input == "Yes" || input == "Y" || input == "yes" || input == "YES" || input == "y")
+            {
+                Console.WriteLine("Lame");
+            }
+            else if (input.ToUpper() == "NO" || input.ToUpper() == "N")
+            {
+                Console.WriteLine("Dude y");
+            }
+            else
+            {
+                Console.WriteLine("tank");
+            }
             Console.ReadKey();
         }
         
